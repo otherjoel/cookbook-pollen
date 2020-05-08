17ce14c4686979662ebc760c7a3a855ab1f1fa7e
Joel Dueck <joel@jdueck.net>
2020-05-07 21:12:59 -0500

    Add rough citations/bibliography tag functions

--- pollen.rkt
+++ pollen.rkt
@@ -3,27 +3,51 @@
 (require racket/list
          pollen/template)
 
-; <span class="tooltip" onclick="this.classList.toggle('tooltip_visible')">
-; &nbsp;+&nbsp;
-; <span class="tooltip-inner"> For demonstration purposes, we’re trapping these errors using wiith-handlers. See <a class="explainer" href="/explainer/errors-and-exceptions.html">errors and exceptions</a>.
-; </span></span>
+(provide ->html (all-defined-out))
 
-(define example
-  '(span [[class "tooltip"]
-          [onclick "this.classList.toggle('tooltip_visible')"]]
-         " + "
-         (span [[class "tooltip-inner"]]
-               "For demonstration purposes, we’re trapping these errors using wiith-handlers")
-         )        
-  )
+;;
+;; Basic tag functions!  ~~~~~~~~~~~~~~~
+;;
+(define (codeblock . elements)
+  `(pre [[class "code"]] ,@elements))
 
-(provide tooltip)
+(define (link url . elements)
+  `(a [[href ,url]] ,@elements))
 
+;;
+;; Citations! Bibliography!  ~~~~~~~~~~~~~~~
+;;
+
+(define citations-table (make-hash))
+
+(define (define-citation key . elems)
+  (hash-set! citations-table key elems))
+
+(define (cite key)
+  (apply tooltip (hash-ref citations-table
+                           key
+                           (list (format "No def for ~a" key)))))
+
+(define (insert-bibliography)
+  ; Get a list of the keys, ensure it is sorted alphabetically
+  ; I had originally converted them to strings before sorting.
+  ; But, turns out Racket has a function I can use with symbols directly 
+  (define keys-srt (sort (hash-keys citations-table) symbol<?))
+  
+  (define list-items
+    (for/list ([item (in-list keys-srt)])
+      `(li ,@(hash-ref citations-table item))))
+  
+  `(@ (h3 "Bibliography") (ol ,@list-items)))
+
+
+;;
+;; Tool tips!  ~~~~~~~~~~~~~~~~~~~~~~~~~~
+;;
 (define (tooltip . elements)
   `(span [[class "tooltip"]
           [onclick "this.classList.toggle('tooltip_visible')"]]
          " + "
          (span [[class "tooltip-inner"]]
-               ,(first elements) )
-         )
-)
+               ,@elements)
+         ))
