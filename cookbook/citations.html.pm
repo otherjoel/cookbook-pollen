e80eaaaeecab6f6f49fa5922559cdfc0ceeb5f48
Joel Dueck <joel@jdueck.net>
2020-07-12 14:41:10 -0500

    Finish draft of citations chapter

--- cookbook/citations.html.pm
+++ cookbook/citations.html.pm
@@ -55,7 +55,8 @@ two options:
 
 ◊li{Construct (and maintain and provide) the information you need in ◊code{pollen.rkt}}
 
-◊li{Defer processing until a later tag function that can see more of the doc (usually ◊code{root})}
+◊li{Save state somewhere and defer processing to a later tag function that can see more of the
+doc (usually ◊code{root})}
 
 }
 
@@ -63,3 +64,66 @@ Both of these approaches are valid and idiomatic. The first is simpler, and suit
 where the same information is used across multiple Pollen sources. But sometimes it isn’t an option;
 sometimes you the information you need can only be found elsewhere in the same document. That’s when
 the second approach is needed.
+
+◊section{Our implementation}
+
+I’m going to explain how this book puts all of the above into practice. Remember, to see the code
+itself, check out the contents of ◊repofile{citations.rkt}.
+
+◊strong{Defining a citation:} The ◊tag{define-citation} tag emits a ◊code{cite-def} X-expression
+with a ◊code{ref} attribute.
+
+◊strong{Referencing a citation:} The ◊tag{cite} tag likewise emits a ◊code{cite} X-expression with a
+◊code{ref} attribute.
+
+◊strong{Inserting a bibliography:} The ◊tag{insert-bibliography} tag inserts ◊code{'(bib)}. That’s
+it, ◊link["https://knowyourmeme.com/memes/thats-it-thats-the-tweet"]{that’s the tweet}.
+
+These tag functions don’t actually do much; they just leave behind little “marker” X-expressions in
+the doc that don’t actually give us the functionality we want — not by themselves.
+
+Recall we’re taking the second approach above: saving the information we’ll need, and deferring
+further processing until the ◊code{root} function. Where do we save the information? We’re saving it
+right in the doc.
+
+Here’s what the doc from our example markup looks like ◊em{after} these tag functions have been
+called, but ◊em{before} the ◊code{root} function is called:
+
+◊codeblock{
+'(root
+  "Paragraphs are important. " (cite [[ref "1"]])
+  (cite-def [[ref "1"]] "Chicago Manual of Style, 15th edition")
+  (bib))
+}
+
+When the ◊code{root} function is called, it has visibility into the entire doc. If you look at this
+function in ◊repofile{pollen.rkt} you’ll see it calls ◊code{citations-root-handler} from
+◊repofile{citations.rkt}. This function does three things:
+
+◊ol{
+
+◊li{Splits out the ◊code{cite-def} X-expressions from the doc using ◊code{splitf-txexpr} and
+converts them into a hash table}
+
+◊li{Using ◊code{decode}, replaces all the ◊code{cite} X-expressions with ◊xref["tooltips"]{tooltips}
+containing the matching entry from the hash table; and replaces any occurrence of ◊code{'(bib)} with
+a list of ◊em{all} the entries from that hash table.}
+
+}
+
+◊section{What else could we do?}
+
+◊ul{
+
+◊li{Omit the ◊tag{insert-bibliography} tag and just insert a bibliography at the end automatically
+if there are any uses of ◊tag{define-citation}.}
+
+◊li{There is a good reason for the ◊tag{cite} tag to leave behind a marker in the document: the
+location in the document where the citation occurred is part of the info we need to keep track of.
+But the same is not true of ◊tag{define-citation}: the location where the citation is defined is not
+useful or needed. So rather than leaving a marker, ◊tag{define-citation} could simply return an
+empty string and add its contents to a hash table. Then you wouldn’t need step 1 above; by the time
+◊code{root} is called, the hash table would be all ready for you and there’d be no ◊code{cite-def}
+X-expressions lying around to clean out of the doc. }
+
+}
