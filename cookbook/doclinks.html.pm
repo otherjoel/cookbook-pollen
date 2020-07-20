f5a07f237cd57686e972828d719a1d2be063de7e
Joel Dueck <joel@jdueck.net>
2020-07-20 07:37:29 -0500

    Refine doclinks chapter a bit

--- cookbook/doclinks.html.pm
+++ cookbook/doclinks.html.pm
@@ -6,20 +6,31 @@
 ◊title{Automatic Racket documentation links}
 
 If you’ve read any Racket documentation at all, you’ve seen how, in every block of example code,
-every function name links directly to the documentation for that function. This is a feature of
+each function name links directly to the documentation for that function. This is a feature of
 ◊link["https://docs.racket-lang.org/scribble/index.html"]{Scribble}, the Racket DSL for creating
 documentation. Scribble exposes this capability so that we can take advantage of it in our programs
 (including Pollen documents) as well.
 
-You can read the source code in ◊repofile{doclinks.rkt}. Roughly, here is what it does when you use
-the ◊tag{rkt} tag function:
+This book implements a ◊tag{rkt} tag function, used like so:
+
+◊codeblock|{
+#lang pollen
+
+Check out the ◊rkt{match} function!
+}|
+
+Which produces this link to the ◊rkt{match} function.
+
+You can read the source code in ◊repofile{doclinks.rkt}. Roughly, here is what the ◊tag{rkt} tag
+function does:
 
 ◊ol{
 
 ◊li{It tries to find a ◊em{definition tag} for the given identifier, which contains information
-about what module provides the documentation and what kind of thing it is. We pass it a list of
-modules to search and call ◊rkt{xref-binding->definition-tag} for each one, stopping on the first
-one that succeeds (using ◊rkt{for/or}).}
+about what module provides the documentation and what kind of thing it is. It uses a pre-defined
+list of modules to search; that way we can limit the search and control the order in which they are
+checked. We call ◊rkt{xref-binding->definition-tag} for each one, stopping on the first one that
+succeeds (using ◊rkt{for/or}).}
 
 ◊li{If we were able to find a definition tag, we can construct a URL from that, using ◊rkt{xref-tag->path+anchor}.}
 
@@ -34,12 +45,15 @@ workaround right now is to uninstall the package and reinstall it in “installa
 ◊section{Quick aside: learning from the Racket documentation}
 
 Ironically, if you wanted to learn how to do ◊em{just this} directly from the Racket documentation,
-it wouldn’t be easy. The Racket docs often provide detailed technical information on individual
-functions without providing examples of how those functions should be used together.◊tooltip{The
+it wouldn’t be easy. You can count on the Racket docs to provide detailed technical information on
+individual functions and even link you to related functions, which is great as far as it goes. But
+they often neglect to provide examples of how those functions should be used together. ◊tooltip{The
 Pollen documentation is a welcome exception.}
 
 When you find yourself in this situation — that is, when you’re pretty sure you’ve found the
 documentation for the functions that do what you need but you’re not gettting the bigger picture —
-the best thing to do is fire up DrRacket, ◊code{require} the modules in question, and try calling
-the function(s) in question. See what results you get, and then try using the output from one as the
-input for another. Looking at the outputs will often clarify things for you.
+the best thing to do is fire up DrRacket, ◊code{require} the necessary modules, and try calling the
+function(s) in question. See what results you get, and then try using the output from one as the
+input for another. Looking at the outputs (in combination with a close reading of the documentation
+that does exist) will put you on a navigable path. The extra work will feel strangely worth it when
+you crack the problem open.
