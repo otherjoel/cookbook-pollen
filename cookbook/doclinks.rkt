92bbd9d616ecd65eadce7228b4802a1f8079e73a
Joel Dueck <joel@jdueck.net>
2020-07-28 23:43:06 -0500

    Make doc links work with user-scope packages (fixes #7)

--- cookbook/doclinks.rkt
+++ cookbook/doclinks.rkt
@@ -1,17 +1,16 @@
 #lang racket/base
 
-(require setup/xref
-         scribble/xref
+(require pkg/path
          racket/list
-         racket/syntax)
+         racket/syntax
+         scribble/xref
+         setup/xref)
 
 (provide modules-to-search
          rkt)
 
 ;; These are the modules that will be checked in order to try and find documentation links.
-;; The packages containing these modules MUST be installed in “installation scope” in order
-;; for this to work (see https://groups.google.com/g/racket-users/c/zIeAaJULzzI)
-(define modules-to-search (make-parameter '(racket txexpr pollen scribble/xref)))
+(define modules-to-search (make-parameter '(racket chess txexpr pollen scribble/xref)))
 
 (define xrefs (load-collections-xref))
 
@@ -24,15 +23,31 @@
                  (lambda _ (for/or ([module-path (in-list (modules-to-search))])
                              (xref-binding->definition-tag xrefs (list module-path identifier) #f)))))))
 
+;; For discussion of why exception handling is needed here,
+;; see https://groups.google.com/g/racket-users/c/zIeAaJULzzI
 (define (racket-docs-link name)
-  (define definition-tag (name->definition-tag name))
+  (define def-tag (name->definition-tag name))
+  (define href
+    (and def-tag
+         (with-handlers
+           ([exn:fail:contract? ; exceptions happen when pkg installed in user scope!
+             (λ _
+               (define-values (p anchor) (xref-tag->path+anchor xrefs def-tag))
+               (define-values (_ subpath) (path->pkg+subpath p))
+               ;; Hack together a URL, should work in most cases
+               (format "~a~a#~a"
+                       "https://docs.racket-lang.org/"
+                       ;; drop `doc/` from the front of the subpath:
+                       (path->string (apply build-path (drop (explode-path subpath) 1)))
+                       anchor))])
+           (define-values (path anchor)
+             (xref-tag->path+anchor xrefs
+                                    def-tag
+                                    #:external-root-url "http://docs.racket-lang.org/"))
+           (format "~a#~a" path anchor))))
   (cond
-    [definition-tag
-      (define-values (path url-tag)
-        (xref-tag->path+anchor xrefs
-                               definition-tag
-                               #:external-root-url "http://docs.racket-lang.org/"))
-      `(a [[href ,(format "~a#~a" path url-tag)]
+    [href
+     `(a [[href ,href]
            [class "rkt-docs"]]
           (code ,name))]
     [else `(code ,name)]))
